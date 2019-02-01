1fe88bf15e31fd0b05ad86d4ddba9cc2b930ee89
unknown <josenogueira95@hotmail.com>
2019-02-01 23:38:46 +0000

    produtos front

--- app/Http/Controllers/Front/ProductsController.php
+++ app/Http/Controllers/Front/ProductsController.php
@@ -8,6 +8,6 @@ use App\Http\Controllers\Controller;
 class ProductsController extends Controller
 {
     public function index(){
-        return view('front.product');
+        return view('front.products');
     }
 }
---
+++ resources/views/front/products.blade.php
@@ -0,0 +1,91 @@
+@extends('layouts.default') @section('content')
+<section>
+    <div class="institutional-banner institutional-banner--masked">
+        <div class="image-bg" style="background-image: url(https://dummyimage.com/1920x900/f2f2f2/000)"></div>
+    </div>
+</section>
+<div class="empty-space-20"></div>
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <ul class="breadcrumb">
+                    <li>
+                        <a href="#home" title="home">Home</a>
+                    </li>
+                    <li>
+                        <a href="products.php" title="Products">Products</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+<div class="empty-space-80"></div>
+<section>
+    <div class="container">
+        <div class="row matchheight">
+            <div class="col-sm-6 col-md-4 col-lg-3" data-mh="product-item">
+                <div class="product-card">
+                    <a href="product.php" title="product title">
+                        <img class="img-responsive" src="https://dummyimage.com/345x345/f2f2f2/000" alt="">
+                    </a>
+                    <a href="product.php" title="product title">
+                        <span class="product-card__category">Category Product</span>
+                        <h4 class="product-card__title">Product Name</h4>
+                    </a>
+                    <span class="product-card__price">10€</span>
+                    <span class="product-card__oldprice">19€</span>
+                    <div class="clearfix"></div>
+                    <a href="product.php" class="btn btn-default">Visualizar</a>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4 col-lg-3" data-mh="product-item">
+                <div class="product-card">
+                    <a href="product.php" title="product title">
+                        <img class="img-responsive" src="https://dummyimage.com/345x345/f2f2f2/000" alt="">
+                    </a>
+                    <a href="product.php" title="product title">
+                        <span class="product-card__category">Category Product</span>
+                        <h4 class="product-card__title">Product Name</h4>
+                    </a>
+                    <span class="product-card__price">10€</span>
+                    <span class="product-card__oldprice">19€</span>
+                    <div class="clearfix"></div>
+                    <a href="product.php" class="btn btn-default">Visualizar</a>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4 col-lg-3" data-mh="product-item">
+                <div class="product-card">
+                    <a href="product.php" title="product title">
+                        <img class="img-responsive" src="https://dummyimage.com/345x345/f2f2f2/000" alt="">
+                    </a>
+                    <a href="product.php" title="product title">
+                        <span class="product-card__category">Category Product</span>
+                        <h4 class="product-card__title">Product Name</h4>
+                    </a>
+                    <span class="product-card__price">10€</span>
+                    <span class="product-card__oldprice">19€</span>
+                    <div class="clearfix"></div>
+                    <a href="product.php" class="btn btn-default">Visualizar</a>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4 col-lg-3" data-mh="product-item">
+                <div class="product-card">
+                    <a href="product.php" title="product title">
+                        <img class="img-responsive" src="https://dummyimage.com/345x345/f2f2f2/000" alt="">
+                    </a>
+                    <a href="product.php" title="product title">
+                        <span class="product-card__category">Category Product</span>
+                        <h4 class="product-card__title">Product Name</h4>
+                    </a>
+                    <span class="product-card__price">10€</span>
+                    <span class="product-card__oldprice">19€</span>
+                    <div class="clearfix"></div>
+                    <a href="product.php" class="btn btn-default">Visualizar</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
